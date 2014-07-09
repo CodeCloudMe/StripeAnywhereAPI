d90d8aeb27517e09604e6285abeffef2159d8f21
Mike <m@m.com>
2014-07-09 19:39:40 -0400

    updated api key for example

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ require(dirname(__FILE__) . '/Stripe/Recipient.php');
 
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here https://manage.stripe.com/account
-Stripe::setApiKey("sk_live_YOUR_KEYHERE!!!!!");
+Stripe::setApiKey("sk_live_4NEAp2LeZdNDWRr9fzP0khXO");
 
 // Get the credit card details submitted by the form
 //$token = $_POST['stripeToken'];
